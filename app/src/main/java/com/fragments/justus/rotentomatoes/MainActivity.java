30fdc5cc15c8dc50d35504c2a658bdddd4876759
justus <jmbaluka@mfarm.co.ke>
2015-06-03 19:08:47 +0300

    rotten tomatoes movies review

---
+++ app/src/main/java/com/fragments/justus/rotentomatoes/MainActivity.java
@@ -0,0 +1,64 @@
+package com.fragments.justus.rotentomatoes;
+
+import android.app.Activity;
+import android.preference.PreferenceActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends Activity {
+    RottenTomatoesClient client;
+    private ListView lvMovies;
+    private BoxOfficeMoviesAdapter adapterMovies;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvMovies = (ListView) findViewById(R.id.lvMovies);
+
+        ArrayList<BoxOfficeMovie> aMovies = new ArrayList<BoxOfficeMovie>();
+        adapterMovies = new BoxOfficeMoviesAdapter(this, aMovies);
+        lvMovies.setAdapter(adapterMovies);
+
+        fetchBoxOfficeMovies();
+    }
+
+    private void fetchBoxOfficeMovies() {
+        client = new RottenTomatoesClient();
+        client.getBoxOfficeMovies(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject responseBody) {
+                JSONArray items = null;
+                try {
+                    // Get the movies json array
+                    items = responseBody.getJSONArray("movies");
+                    // Parse json array into array of model objects
+                    ArrayList<BoxOfficeMovie> movies = BoxOfficeMovie.fromJson(items);
+                    // Load model objects into the adapter
+                    for (BoxOfficeMovie movie : movies) {
+                        adapterMovies.add(movie); // add movie through the adapter
+                    }
+                    adapterMovies.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
+}
